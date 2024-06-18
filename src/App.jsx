b1ae652e6c84b572e825a4b2c6d09873a4d42ba3
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-06-18 09:16:37 +0200

    ajout parallax mousemove

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import './App.css'
+import { useState, useEffect } from 'react';
 import ChickenParallax from './components/ChickenParallax'
 function App() {
+
+
+  //mousePosition est l'état qui contiendra les coordonnées x et y de la souris.
+  //setMousePosition est la fonction utilisée pour mettre à jour cet état.
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  //e est l'événement de la souris, un objet qui contient des informations sur l'événement, 
+  //y compris les coordonnées actuelles de la souris (e.clientX et e.clientY).
+  const handleMouseMove = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+};
+
+
+useEffect(() => {
+  window.addEventListener('mousemove', handleMouseMove);
+  return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+  };
+}, []);
+
+const parallaxH1Style = {
+  transform: `translate(${mousePosition.x * -0.15}px, ${mousePosition.y * -0.15}px)`,
+};
 
   return (
     <>
     <ChickenParallax />
       <div id='contentContainer'>
           <section>
-            <h2>Parallax Scrolling Website</h2>
+            <h2 style={parallaxH1Style}>Parallax Scrolling Website</h2>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda repudiandae modi autem, hic sunt alias aspernatur laboriosam voluptas consectetur possimus reprehenderit cupiditate quia amet incidunt molestiae pariatur ipsa doloribus nihil!</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nisi assumenda, iste, eum adipisci incidunt nemo consequatur, voluptatem quisquam inventore repellat! Nihil praesentium natus repudiandae explicabo magnam incidunt officia, illo, porro consectetur est hic animi autem obcaecati sequi dolores, accusamus quidem dolore dicta fuga ab minus aliquam similique nisi? Ducimus, accusamus. Iste veniam quo enim ex nesciunt, eaque ut corporis in possimus modi fugit et porro dignissimos laudantium eum, voluptas consequuntur cum qui libero perferendis quam eveniet asperiores? Eveniet cumque mollitia aperiam repellat temporibus asperiores omnis quidem sapiente ipsum quisquam odit eligendi accusamus nobis quis laborum id sed, quasi cupiditate necessitatibus quia vero perspiciatis! Dolore, nemo cumque dicta quia exercitationem natus laboriosam deserunt rerum ab blanditiis, nobis quod odio cum suscipit ullam consequuntur. Itaque in animi quibusdam nostrum mollitia. Quos unde laborum dignissimos deserunt cupiditate placeat, reprehenderit excepturi, illo nisi ea ab, laudantium repudiandae ratione corporis doloremque! Voluptas perferendis quam enim. Possimus corporis aspernatur accusamus quaerat obcaecati commodi atque dolores, similique tenetur veritatis, quas magnam libero autem illo eveniet, voluptatibus deserunt? Iusto obcaecati aut dolorem exercitationem nemo magni nam odit officiis ipsa, rem eius quaerat neque dolorum, modi dolor dignissimos earum eligendi harum accusantium quisquam ab animi esse. Ducimus, laboriosam!</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium corporis veniam totam commodi voluptatem unde laudantium ipsum perspiciatis asperiores enim. Delectus sunt enim mollitia sit excepturi, dicta adipisci voluptas odit natus temporibus dolore vel minima fugiat, aperiam in, a nihil dolor repellendus nobis consequuntur eveniet corporis ullam porro. Ad ea aut rem dolorum blanditiis consectetur voluptatum optio asperiores maiores id autem laborum officiis, sit facere at architecto quod non iste minima iusto, a natus ipsam enim. Necessitatibus ducimus et quaerat neque hic. Debitis, maxime, sed labore dignissimos quia animi nulla dolore minima tempore consequuntur tempora? Soluta ipsum magnam suscipit quis pariatur ducimus asperiores? Molestias omnis velit consequuntur illo, enim perspiciatis sint, eum voluptatum tenetur voluptas deserunt eos iure adipisci pariatur id soluta laudantium minima magnam quod, voluptate quidem. Consequuntur excepturi ipsa quia nam perferendis sapiente aspernatur iure eos. Debitis facere impedit veritatis pariatur enim nisi, amet aliquid a blanditiis aspernatur mollitia consequuntur. Soluta explicabo minus dicta sequi inventore alias et possimus facere quas quae exercitationem dolorem aliquid unde quos voluptas corrupti sed, quia distinctio suscipit ex blanditiis molestiae quam rem sapiente! Perspiciatis sint, delectus pariatur debitis voluptatibus recusandae inventore laudantium modi, maiores illo odit eaque adipisci dolores ad sequi quisquam?</p>
